14c928ea9318171781ff4de0f034947251fd746c
Sam Morgan <sam@plugable.com>
2022-01-21 13:55:57 -0800

    Increment version major release

--- helpjuice/__version__.py
+++ helpjuice/__version__.py
@@ -4,7 +4,7 @@
 __title__ = "helpjuice"
 __description__ = "Python Helpjuice API Wrapper."
 __url__ = "https://helpjuice.readthedocs.io"
-__version__ = "0.1.4"
+__version__ = "3.0.0"
 __author__ = "Sam Morgan"
 __author_email__ = "sam@samamorgan.com"
 __license__ = "GNU GPL 3.0"
